585e53143c340fd08bfbe879426abca667a86982
Dennis Porto <60332417+dennisreis-dev@users.noreply.github.com>
2022-11-15 21:48:55 -0300

    Deletando arquivos

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const btnMobile = getElementById = document.getElementById('btn-mobile');
-
-function toggleMenu () {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
-
-btnMobile.addEventListener('click', toggleMenu);
